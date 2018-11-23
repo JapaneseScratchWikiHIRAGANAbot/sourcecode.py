919918fd15f37b092ddcacb881e41e87a4cb41b2
T-taku <39856622+T-taku@users.noreply.github.com>
2018-11-23 13:53:29 +0900

    Update source.py

--- source.py
+++ source.py
@@ -2,10 +2,10 @@ import mw_api_client as mwc
 from pykakasi import kakasi,wakati
 wp = mwc.Wiki("https://ja.scratch-wiki.info/w/api.php")
 wp.login("T-taku@T-takumini", "pass")
-sandbox = wp.page('記事')
+sandbox = wp.page('記事')#記事
 contents = sandbox.read()
 kakasi = kakasi()
 kakasi.setMode("J","H")
 text = (contents)
-conv = kakasi.getConverter()
+conv = kakasi.getConverter()#ひらがな化
 print(conv.do(text))
